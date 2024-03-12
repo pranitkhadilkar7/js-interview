165d2c1d62c753d5ad9e4540733a6162546a8d4a
Pranit Khadilkar <pranit@emtech.com>
2024-03-12 17:21:58 +0530

    scope chain

---
+++ 08. scope-chain.js
@@ -0,0 +1,23 @@
+'use strict'
+
+function calcAge (birthYear) {
+    const age = 2024 - birthYear
+
+    function printAge () {
+        const output = `${firstName}, you are ${age}, born in ${birthYear}` // dont give error
+        console.log(output)
+
+        if (birthYear >= 1981 && birthYear <- 1996) {
+            var millenial = true
+            const str = `Oh, and you are millenial, ${firstName}`
+            console.log(str)
+        }
+        // console.log(str) --> this gives error as str is declared with const which is block scoped. if declared with var then it wont give error
+        console.log(millenial) // it won't give error
+    }
+}
+
+const firstName = 'Pranit'
+
+// console.log(age) gives error as age is not in the current or parent scope
+// printAge() gives error as printAge is not in the current or parent scope
