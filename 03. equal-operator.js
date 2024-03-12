d9e82d53914380aa8ecb8958d563671c4d85586d
Pranit Khadilkar <pranit@emtech.com>
2024-03-12 15:49:06 +0530

    equals to  operator

---
+++ 03. equal-operator.js
@@ -0,0 +1,9 @@
+// there are 2 equal operators
+// === and ==
+// === is strict equal. It does not do type coherison
+// == is loose equal. It does type cohersion
+
+console.log(18 === 18) // true
+console.log('18' === 18) // false
+console.log(18 == 18) // true
+console.log('18' == 18) // true. because '18' is converted to integer 18 before comparing
