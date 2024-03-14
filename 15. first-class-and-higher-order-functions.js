365fff4f8bd585278db8a83e1d09d5570c3d790c
Pranit Khadilkar <pranit@emtech.com>
2024-03-14 11:46:58 +0530

    first class and higher order function

---
+++ 15. first-class-and-higher-order-functions.js
@@ -0,0 +1,8 @@
+// First class function is just a fact that function are just type of object
+
+const add = (a, b) => a + b
+
+console.log(typeof add) // function
+
+// Higher order functions are the function which takes function as argument
+// and returns another function
